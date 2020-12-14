b79339bfa51004f026852e3ea09aa0de3bf7c597
IlonaDemcenko <64262534+IlonaDemcenko@users.noreply.github.com>
2020-12-14 19:11:50 +0200

    Pieliku tabula.css un tabula.js. JS nestrāda

---
+++ tabula.js
@@ -0,0 +1,22 @@
+document.getElementById("demo").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
+}
+
+/*const PogaIzpilditi = document.getElementById('PogaIzpilditi');
+
+
+//const PieteiktasRindas = document.getElementsByClassName('pieteikums');
+PogaIzpilditi.addEventListener('click', function() {
+
+    document.getElementById("PogaIzpilditi").innerHTML = "YOU CLICKED ME!";
+  /*  PogaIzpilditi.classList.toggle('selected');
+    for (let i = 0; i < PieteiktasRindas.length; i++) {
+    PieteiktasRindas[i].classList.toggle('slepts');
+    }
+  
+
+});
+
+*/
