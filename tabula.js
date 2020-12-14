2b6c8c6f337815357f63be9d1a78ed6434280655
IlonaDemcenko <64262534+IlonaDemcenko@users.noreply.github.com>
2020-12-14 20:47:16 +0200

    Strāda 2 pogu filtri

--- tabula.js
+++ tabula.js
@@ -1,22 +1,30 @@
-document.getElementById("demo").addEventListener("click", myFunction);
+function init () {
 
-function myFunction() {
-  document.getElementById("demo").innerHTML = "YOU CLICKED ME!";
-}
+const PogaIzpilditi = document.getElementById('PogaIzpilditi');
+const PieteiktasRindas = document.getElementsByClassName('pieteikums');
 
-/*const PogaIzpilditi = document.getElementById('PogaIzpilditi');
-
-
-//const PieteiktasRindas = document.getElementsByClassName('pieteikums');
 PogaIzpilditi.addEventListener('click', function() {
 
-    document.getElementById("PogaIzpilditi").innerHTML = "YOU CLICKED ME!";
-  /*  PogaIzpilditi.classList.toggle('selected');
+
+    PogaIzpilditi.classList.toggle('selected');
     for (let i = 0; i < PieteiktasRindas.length; i++) {
-    PieteiktasRindas[i].classList.toggle('slepts');
+      PieteiktasRindas[i].classList.toggle('slepts');
     }
   
-
 });
 
-*/
+const PogaNepabeigti = document.getElementById('PogaNepabeigti');
+const IzpilditasRindas = document.getElementsByClassName('izpildits');
+
+PogaNepabeigti.addEventListener('click', function() {
+
+
+    PogaNepabeigti.classList.toggle('selected');
+    for (let i = 0; i < IzpilditasRindas.length; i++) {
+      IzpilditasRindas[i].classList.toggle('slepts');
+    }
+  
+});
+
+
+}
