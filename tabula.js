ae1a1e68a84d88e475cf2a9fa93b47bd958399d0
IlonaDemcenko <64262534+IlonaDemcenko@users.noreply.github.com>
2020-12-18 17:26:31 +0200

    Pieliku reakciju uz pogu "Nejaušie dati"

--- tabula.js
+++ tabula.js
@@ -26,5 +26,39 @@ PogaNepabeigti.addEventListener('click', function() {
   
 });
 
+const PogaVisi = document.getElementById('PogaVisi');
+PogaVisi.addEventListener('click', function(){
+
+    const kabineti = ["11. kab", "Lielais koridors", "352.kab.", "111.kab.", "102.kab", "119.kab.", "Zāle"];
+    const apraksti = ["Nedeg lampiņa", "Neslēdzas durvis", "Nestrādā kaut kas", "Salauzsts krēsls", "Bojāta siena"];
+    
+    let rinduSkaits = Math.floor(Math.random() * 3)+5;
+
+    let tabulasRindas = document.querySelector('.rindas');
+    tabulasRindas.innerHTML = '';
+    for (let i = 0; i < rinduSkaits; i++)
+       {
+        let randKorp = Math.floor(Math.random() * 4);
+        let randKab = Math.floor(Math.random() * kabineti.length);
+        let randApr = Math.floor(Math.random() * apraksti.length);
+        tabulasRindas.innerHTML += 
+        `<tr>
+        <td>${i+1}.</td>
+        <td>${randKorp+1}. Korpuss</td>
+        <td>${Math.floor(Math.random() * 30)+1}.${Math.floor(Math.random() * 12)+1}.2020</td>
+        <td>${kabineti[randKab]}</td>
+        <td>${apraksti[randApr]}</td>
+        `;
+       }
+  
+  
+});
+
 
 }
+
+
+
+// beidzas funkcija init(), kura gaida, kamēr ieladēsies html lapa
+
+
